0e90789d6e710032dfa9b82ebcd21ffbb17118d0
leeseojune <sung07288346@gmail.com>
2021-07-20 22:55:08 +0900

    init project

---
+++ src/main/java/io/github/opgg/music_ward_server/MusicWardServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.opgg.music_ward_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicWardServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicWardServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/opgg/music_ward_server/MusicWardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.opgg.music_ward_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicWardServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
