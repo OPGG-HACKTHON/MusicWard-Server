f77041364ecf96de71e778fb3a9f9a4b008dd75e
leeseojune <sung07288346@gmail.com>
2021-08-11 22:20:19 +0900

    Add :: Token Entity 작성

---
+++ src/main/java/io/github/opgg/music_ward_server/entity/token/Token.java
@@ -0,0 +1,31 @@
+package io.github.opgg.music_ward_server.entity.token;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+@Getter
+@AllArgsConstructor
+@RedisHash
+public class Token {
+
+    @Id
+    private final Long id;
+
+    private final Type type;
+
+    private String refreshToken;
+
+    @TimeToLive
+    private Long ttl;
+
+    public Token update(String refreshToken, Long ttl) {
+        this.refreshToken = refreshToken;
+        this.ttl = ttl;
+        return this;
+    }
+
+
+}
