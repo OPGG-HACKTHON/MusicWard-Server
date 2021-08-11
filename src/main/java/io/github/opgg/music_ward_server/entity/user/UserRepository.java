7539955d14af0e9c65713eb1a228226fa0bca5fb
leeseojune <sung07288346@gmail.com>
2021-08-11 23:33:37 +0900

    Update :: 불필요한 JpaRepository를 CrudRepository로 변경

--- src/main/java/io/github/opgg/music_ward_server/entity/user/UserRepository.java
+++ src/main/java/io/github/opgg/music_ward_server/entity/user/UserRepository.java
@@ -1,8 +1,9 @@
 package io.github.opgg.music_ward_server.entity.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 }
