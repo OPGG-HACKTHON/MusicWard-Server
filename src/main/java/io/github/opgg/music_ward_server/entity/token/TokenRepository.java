2979114bb522fd76e4c30854d79a86717a260a07
leeseojune <sung07288346@gmail.com>
2021-08-11 22:20:25 +0900

    Add :: Token DAO

---
+++ src/main/java/io/github/opgg/music_ward_server/entity/token/TokenRepository.java
@@ -0,0 +1,11 @@
+package io.github.opgg.music_ward_server.entity.token;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TokenRepository extends CrudRepository<Token, Long> {
+    Optional<Token> findByIdAndType(Long id, Type type);
+}
