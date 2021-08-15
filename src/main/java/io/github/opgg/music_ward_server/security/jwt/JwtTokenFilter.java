2297c4ed9ccc2ee06e223307fc6b4c12ff610708
leeseojune <sung07288346@gmail.com>
2021-08-15 18:41:04 +0900

    Update :: 필터 구현

--- src/main/java/io/github/opgg/music_ward_server/security/jwt/JwtTokenFilter.java
+++ src/main/java/io/github/opgg/music_ward_server/security/jwt/JwtTokenFilter.java
@@ -1,6 +1,10 @@
 package io.github.opgg.music_ward_server.security.jwt;
 
+import io.github.opgg.music_ward_server.exception.ExpiredAccessTokenException;
+import io.github.opgg.music_ward_server.exception.InvalidTokenException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,7 +22,15 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+        try {
+            String token = jwtTokenProvider.resolveToken(request);
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication authentication = jwtTokenProvider.authentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (InvalidTokenException e) {
+            SecurityContextHolder.clearContext();
+        }
+        filterChain.doFilter(request, response);
     }
-
 }
