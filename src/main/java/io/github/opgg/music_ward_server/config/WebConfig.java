36d501c9c6c0a876bb7861a53491462354144a0a
leeseojune <sung07288346@gmail.com>
2021-08-15 18:04:08 +0900

    Update :: OPTIONS

--- src/main/java/io/github/opgg/music_ward_server/config/WebConfig.java
+++ src/main/java/io/github/opgg/music_ward_server/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedOrigins("http://localhost:3000");
     }
 
