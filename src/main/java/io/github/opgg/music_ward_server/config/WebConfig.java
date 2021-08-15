adbe8e7a8f82df6adb0ad731d2df23c0f66d666d
leeseojune <sung07288346@gmail.com>
2021-08-15 17:58:12 +0900

    Update :: CORS 메소드 추가

--- src/main/java/io/github/opgg/music_ward_server/config/WebConfig.java
+++ src/main/java/io/github/opgg/music_ward_server/config/WebConfig.java
@@ -10,6 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedOrigins("http://localhost:3000");
     }
+
 }
