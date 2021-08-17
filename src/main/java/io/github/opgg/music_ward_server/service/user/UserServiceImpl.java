17d6c3f2c76b6e212cb11ab292b52393756a9447
leeseojune <sung07288346@gmail.com>
2021-08-17 23:30:26 +0900

    Update :: getGoogleLink 구현

--- src/main/java/io/github/opgg/music_ward_server/service/user/UserServiceImpl.java
+++ src/main/java/io/github/opgg/music_ward_server/service/user/UserServiceImpl.java
@@ -1,7 +1,26 @@
 package io.github.opgg.music_ward_server.service.user;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private static final String GOOGLE_LOGIN_LINK = "https://accounts.google.com/o/oauth2/v2/auth";
+
+    @Value("${oauth.google.client_id}")
+    private String clientId;
+
+    @Value("${oauth.redirect_uri}")
+    private String redirectUri;
+
+    @Override
+    public String getGoogleLink() {
+        return GOOGLE_LOGIN_LINK +
+                "?client_id=" + clientId +
+                "&scope=https%3A//www.googleapis.com/auth/userinfo.email" +
+                "&response_type=code" +
+                "&redirect_uri=" + redirectUri;
+    }
+
 }
