5980625ed6ab40460afac94a52e8335434e1b701
leeseojune <sung07288346@gmail.com>
2021-08-17 23:22:42 +0900

    Add :: UserController

---
+++ src/main/java/io/github/opgg/music_ward_server/controller/UserController.java
@@ -0,0 +1,12 @@
+package io.github.opgg.music_ward_server.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+
+
+}
