e58fbc703afffadc4854451b6751f5048d168808
leeseojune <sung07288346@gmail.com>
2021-08-20 19:10:53 +0900

    ⚡ :: getTokenByCode

--- src/main/java/io/github/opgg/music_ward_server/service/user/UserService.java
+++ src/main/java/io/github/opgg/music_ward_server/service/user/UserService.java
@@ -1,5 +1,9 @@
 package io.github.opgg.music_ward_server.service.user;
 
+import io.github.opgg.music_ward_server.dto.user.response.GoogleLinkResponse;
+import io.github.opgg.music_ward_server.dto.user.response.TokenResponse;
+
 public interface UserService {
-    String getGoogleLink();
+    GoogleLinkResponse getGoogleLink();
+    TokenResponse getTokenByCode(String code);
 }
