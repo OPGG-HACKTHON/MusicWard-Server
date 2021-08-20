41310ccdfe03b529b03df0d40a8ff07cdee3fe76
hyeonic <evan3566@gmail.com>
2021-08-21 02:55:04 +0900

    Add :: CredentialsNotFoundException 추가

--- src/main/java/io/github/opgg/music_ward_server/exception/CredentialsNotFoundException.java
+++ src/main/java/io/github/opgg/music_ward_server/exception/CredentialsNotFoundException.java
@@ -1,2 +1,11 @@
-package io.github.opgg.music_ward_server.error.exception;public class CredentialsNotFoundException {
+package io.github.opgg.music_ward_server.exception;
+
+import io.github.opgg.music_ward_server.error.exception.ErrorCode;
+import io.github.opgg.music_ward_server.error.exception.MusicWardException;
+
+public class CredentialsNotFoundException extends MusicWardException {
+
+    public CredentialsNotFoundException() {
+        super(ErrorCode.CREDENTIALS_NOT_FOUND);
+    }
 }
