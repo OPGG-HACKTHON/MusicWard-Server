72dc95bb0110e416ec30488c864a945caf03b499
hyeonic <evan3566@gmail.com>
2021-08-21 02:54:43 +0900

    Add :: SecurityUtil 추가

---
+++ src/main/java/io/github/opgg/music_ward_server/utils/security/SecurityUtil.java
@@ -0,0 +1,26 @@
+package io.github.opgg.music_ward_server.utils.security;
+
+import io.github.opgg.music_ward_server.entity.user.User;
+import io.github.opgg.music_ward_server.exception.CredentialsNotFoundException;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SecurityUtil {
+
+    public static Long getCurrentUserId() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || authentication.getPrincipal() == null) {
+            throw new CredentialsNotFoundException();
+        }
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        User user = (User) userDetails;
+
+        return user.getId();
+    }
+}
