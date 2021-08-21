6791945f9f1c6842ecc2e2fc74c07864d86bd211
leeseojune <sung07288346@gmail.com>
2021-08-21 23:28:06 +0900

    ⚡ :: 요청 url 변경

--- src/main/java/io/github/opgg/music_ward_server/utils/api/client/GoogleInfoClient.java
+++ src/main/java/io/github/opgg/music_ward_server/utils/api/client/GoogleInfoClient.java
@@ -5,10 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(value = "googleInfoClient", url = "https://www.googleapis.com/oauth2")
+@FeignClient(value = "googleInfoClient", url = "https://openidconnect.googleapis.com")
 public interface GoogleInfoClient {
 
-    @GetMapping("/v3/userinfo")
+    @GetMapping("/v1/userinfo")
     GoogleEmailResponse getEmail(@RequestHeader("Authorization") String accessToken);
 
 }
