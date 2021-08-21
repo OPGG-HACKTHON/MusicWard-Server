77c8558caf5e0779bb1aa52378f38aad1c70770e
hyeonic <evan3566@gmail.com>
2021-08-21 21:37:57 +0900

    Add :: 등록하지 않은 playlists 목록 구현

--- src/main/java/io/github/opgg/music_ward_server/dto/playlist/NonPlaylistsResponse.java
+++ src/main/java/io/github/opgg/music_ward_server/dto/playlist/NonPlaylistsResponse.java
@@ -1,4 +1,56 @@
-package io.github.opgg.music_ward_server.dto;
+package io.github.opgg.music_ward_server.dto.playlist;
 
-public class NonPlaylists {
+import io.github.opgg.music_ward_server.entity.playlist.Provider;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+public class NonPlaylistsResponse {
+
+    private Provider provider;
+    private List<Playlist> items;
+
+    @Builder
+    public NonPlaylistsResponse(Provider provider, List<Playlist> items) {
+        this.provider = provider;
+        this.items = items;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class Playlist {
+        private String originalId;
+        private String originalTitle;
+        private String originalDescription;
+        private Image image;
+        private String externalUrl;
+
+        @Builder
+        public Playlist(String originalId, String originalTitle, String originalDescription,
+                        Image image, String externalUrl) {
+            this.originalId = originalId;
+            this.originalTitle = originalTitle;
+            this.originalDescription = originalDescription;
+            this.image = image;
+            this.externalUrl = externalUrl;
+        }
+
+        @Getter
+        @NoArgsConstructor
+        public static class Image {
+            private String url;
+            private String width;
+            private String height;
+
+            @Builder
+            public Image(String url, String width, String height) {
+                this.url = url;
+                this.width = width;
+                this.height = height;
+            }
+        }
+    }
 }
