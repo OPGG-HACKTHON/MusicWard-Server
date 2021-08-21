6302bf3479d9ab615722ad6f224649f2be3b5f7a
hyeonic <evan3566@gmail.com>
2021-08-21 15:53:42 +0900

    Add :: getAccessToken 메소드 추가

--- src/main/java/io/github/opgg/music_ward_server/service/user/UserService.java
+++ src/main/java/io/github/opgg/music_ward_server/service/user/UserService.java
@@ -2,8 +2,10 @@ package io.github.opgg.music_ward_server.service.user;
 
 import io.github.opgg.music_ward_server.dto.user.response.GoogleLinkResponse;
 import io.github.opgg.music_ward_server.dto.user.response.TokenResponse;
+import io.github.opgg.music_ward_server.utils.api.dto.google.GoogleAccessTokenResponse;
 
 public interface UserService {
     GoogleLinkResponse getGoogleLink();
     TokenResponse getTokenByCode(String code);
+    GoogleAccessTokenResponse getAccessToken(String refreshToken);
 }
