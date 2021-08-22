5fb13b43bc0a645469d8a28346c6cbddad63b0dc
hyeonic <dev.hyeonic@gmail.com>
2021-08-23 01:09:30 +0900

    Add :: playlist service 메소드 추가

--- src/main/java/io/github/opgg/music_ward_server/service/playlist/PlaylistService.java
+++ src/main/java/io/github/opgg/music_ward_server/service/playlist/PlaylistService.java
@@ -1,7 +1,10 @@
 package io.github.opgg.music_ward_server.service.playlist;
 
 import io.github.opgg.music_ward_server.dto.playlist.NonPlaylistsResponse;
+import io.github.opgg.music_ward_server.dto.playlist.PlaylistMainResponse;
+import io.github.opgg.music_ward_server.dto.playlist.PlaylistSaveRequest;
 
 public interface PlaylistService {
+    PlaylistMainResponse save(PlaylistSaveRequest request);
     NonPlaylistsResponse getNonPlaylists();
 }
