18f4ebaa7eb65a5fa7aa33e61aba196f7d35b92d
leeseojune <sung07288346@gmail.com>
2021-08-22 23:26:16 +0900

    Update :: 이름 변경

--- src/main/java/io/github/opgg/music_ward_server/utils/api/dto/google/GoogleCodeRequest.java
+++ src/main/java/io/github/opgg/music_ward_server/utils/api/dto/google/GoogleCodeRequest.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class CodeRequest {
+public class GoogleCodeRequest {
 
     private String code;
     private String clientId;
