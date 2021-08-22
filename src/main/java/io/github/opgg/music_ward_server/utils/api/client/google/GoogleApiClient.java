3b96820d58178c85df67ef9ce36b2013b714726a
hyeonic <dev.hyeonic@gmail.com>
2021-08-23 01:06:58 +0900

    Add :: youtube playlist 단건 조회 추가

--- src/main/java/io/github/opgg/music_ward_server/utils/api/client/google/GoogleApiClient.java
+++ src/main/java/io/github/opgg/music_ward_server/utils/api/client/google/GoogleApiClient.java
@@ -1,5 +1,6 @@
 package io.github.opgg.music_ward_server.utils.api.client.google;
 
+import io.github.opgg.music_ward_server.utils.api.dto.google.YoutubePlaylistResponse;
 import io.github.opgg.music_ward_server.utils.api.dto.google.YoutubePlaylistsResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "googleApiClient", url = "https://www.googleapis.com")
 public interface GoogleApiClient {
+
+    @GetMapping("/youtube/v3/playlistItems")
+    YoutubePlaylistResponse getPlaylist(@RequestHeader("Authorization") String token,
+                                        @RequestParam("part") String part,
+                                        @RequestParam("playlistId") String playlistId,
+                                        @RequestParam("maxResults") String maxResults);
 
     @GetMapping("/youtube/v3/playlists")
     YoutubePlaylistsResponse getPlaylists(@RequestHeader("Authorization") String token,
