29d0a2399362f88a7c3ee763d8bc2d17b347cc34
hyeonic <dev.hyeonic@gmail.com>
2021-08-23 01:11:07 +0900

    Add :: playlist controller save 메소드 추가

--- src/main/java/io/github/opgg/music_ward_server/controller/PlaylistController.java
+++ src/main/java/io/github/opgg/music_ward_server/controller/PlaylistController.java
@@ -2,14 +2,17 @@ package io.github.opgg.music_ward_server.controller;
 
 import io.github.opgg.music_ward_server.controller.response.CommonResponse;
 import io.github.opgg.music_ward_server.dto.playlist.NonPlaylistsResponse;
+import io.github.opgg.music_ward_server.dto.playlist.PlaylistSaveRequest;
 import io.github.opgg.music_ward_server.service.playlist.PlaylistService;
-import io.github.opgg.music_ward_server.utils.api.dto.google.YoutubePlaylistsResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 
 @RequestMapping("/")
 @RequiredArgsConstructor
@@ -24,5 +27,11 @@ public class PlaylistController {
         NonPlaylistsResponse nonPlaylists = playlistService.getNonPlaylists();
 
         return ResponseEntity.ok(new CommonResponse(nonPlaylists));
+    }
+
+    @PostMapping("/playlists")
+    public ResponseEntity<CommonResponse> save(@Valid @RequestBody PlaylistSaveRequest requestDto) {
+
+        return ResponseEntity.ok(new CommonResponse(playlistService.save(requestDto)));
     }
 }
