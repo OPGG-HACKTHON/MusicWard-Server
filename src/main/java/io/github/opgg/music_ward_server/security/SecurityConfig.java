f6cd33305458e6893c47558b5fcdf5dcf1344c3e
hyeonic <dev.hyeonic@gmail.com>
2021-08-23 14:56:41 +0900

    Add :: controller 추가 및 접근 허용

--- src/main/java/io/github/opgg/music_ward_server/security/SecurityConfig.java
+++ src/main/java/io/github/opgg/music_ward_server/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/auth/spotify").permitAll()
                 .antMatchers(HttpMethod.GET,"/championlist").permitAll()
                 .antMatchers(HttpMethod.GET,"/champion/{championId}").permitAll()
-                .antMatchers(HttpMethod.GET,"/playlists").permitAll()
+                .antMatchers(HttpMethod.GET,"/playlists/**").permitAll()
                 .anyRequest().authenticated()
                 .and().apply(new FilterConfigure(jwtTokenProvider, exceptionHandlerFilter));
     }
