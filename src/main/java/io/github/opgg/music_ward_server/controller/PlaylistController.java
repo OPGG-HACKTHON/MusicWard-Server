2a738c4106cf7aefbeb614dda5f5ebb6176890a9
hyeonic <dev.hyeonic@gmail.com>
2021-08-23 16:23:52 +0900

    Add :: playlist 수정을 위한 api 추가

--- src/main/java/io/github/opgg/music_ward_server/controller/PlaylistController.java
+++ src/main/java/io/github/opgg/music_ward_server/controller/PlaylistController.java
@@ -2,6 +2,7 @@ package io.github.opgg.music_ward_server.controller;
 
 import io.github.opgg.music_ward_server.controller.response.CommonResponse;
 import io.github.opgg.music_ward_server.dto.playlist.request.PlaylistSaveRequest;
+import io.github.opgg.music_ward_server.dto.playlist.request.PlaylistUpdateRequest;
 import io.github.opgg.music_ward_server.service.playlist.PlaylistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +44,15 @@ public class PlaylistController {
 
     @GetMapping("/playlists/{playlistId}")
     public ResponseEntity<CommonResponse> findById(@PathVariable("playlistId") Long playlistId) {
+
+        return new ResponseEntity<>(new CommonResponse(playlistService.findById(playlistId)), HttpStatus.OK);
+    }
+
+    @PutMapping("/playlists/{playlistId}")
+    public ResponseEntity<CommonResponse> update(@PathVariable("playlistId") Long playlistId,
+                                                 @RequestBody PlaylistUpdateRequest updateDto) {
+
+        playlistService.update(playlistId, updateDto);
 
         return new ResponseEntity<>(new CommonResponse(playlistService.findById(playlistId)), HttpStatus.OK);
     }
