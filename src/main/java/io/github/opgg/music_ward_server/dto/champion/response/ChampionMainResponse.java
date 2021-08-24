d2714514603c62d11834ee7d3f965c5345a99ee0
hyeonic <dev.hyeonic@gmail.com>
2021-08-25 00:16:53 +0900

    Update :: response dto 수정

--- src/main/java/io/github/opgg/music_ward_server/dto/champion/response/ChampionMainResponse.java
+++ src/main/java/io/github/opgg/music_ward_server/dto/champion/response/ChampionMainResponse.java
@@ -1,6 +1,5 @@
 package io.github.opgg.music_ward_server.dto.champion.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.github.opgg.music_ward_server.entity.champion.Champion;
