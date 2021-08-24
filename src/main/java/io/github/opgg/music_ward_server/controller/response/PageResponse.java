6062b3d507bc9ad230264c963ea5ba7356e6b2d2
hyeonic <dev.hyeonic@gmail.com>
2021-08-24 16:54:59 +0900

    Add :: controller 수정 및 공통 처리를 위한 PageResponse 추가

--- src/main/java/io/github/opgg/music_ward_server/controller/response/PageResponse.java
+++ src/main/java/io/github/opgg/music_ward_server/controller/response/PageResponse.java
@@ -1,2 +1,17 @@
-package io.github.opgg.music_ward_server.controller.response;public class PageResponse {
+package io.github.opgg.music_ward_server.controller.response;
+
+import io.github.opgg.music_ward_server.dto.page.response.PageInfoResponse;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PageResponse<T> extends CommonResponse {
+
+    PageInfoResponse pageInfo;
+
+    public PageResponse(T data, PageInfoResponse pageInfoResponse) {
+        super(data);
+        this.pageInfo = pageInfoResponse;
+    }
 }
