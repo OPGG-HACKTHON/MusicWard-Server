d33ddaf96d91699431f8f3679c84689388b36f26
DESKTOP-PNLTC14\IRC <save3172@naver.com>
2021-08-26 04:40:05 +0900

    Update :: champion response dto @JsonNaming 추가 및 수정

--- src/main/java/io/github/opgg/music_ward_server/dto/champion/response/ChampionListResponse.java
+++ src/main/java/io/github/opgg/music_ward_server/dto/champion/response/ChampionListResponse.java
@@ -1,9 +1,12 @@
 package io.github.opgg.music_ward_server.dto.champion.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import java.util.List;
 
 @Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ChampionListResponse {
 
     private List<ChampionListDTO> championList;
