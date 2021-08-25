ba1e9db614cd557e9f3d267e8d04843450e600d0
leeseojune <sung07288346@gmail.com>
2021-08-25 19:31:00 +0900

    Update :: deleteByUser추가

--- src/main/java/io/github/opgg/music_ward_server/entity/ward/WardRepository.java
+++ src/main/java/io/github/opgg/music_ward_server/entity/ward/WardRepository.java
@@ -1,11 +1,12 @@
 package io.github.opgg.music_ward_server.entity.ward;
 
+import io.github.opgg.music_ward_server.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WardRepository extends JpaRepository<Ward, Long> {
-
     Integer countByPlaylistId(Long playlistId);
     void deleteByPlaylistId(Long playlistId);
+    void deleteByUser(User user);
 }
