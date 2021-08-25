ff9575217d9049c22998f9e65d7151f32fdc8595
leeseojune <sung07288346@gmail.com>
2021-08-25 23:09:08 +0900

    Add :: 댓글 등록 Request Dto

---
+++ src/main/java/io/github/opgg/music_ward_server/dto/comment/request/CommentRequest.java
@@ -0,0 +1,16 @@
+package io.github.opgg.music_ward_server.dto.comment.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class CommentRequest {
+
+    private String comment;
+    private Integer playlistId;
+
+}
