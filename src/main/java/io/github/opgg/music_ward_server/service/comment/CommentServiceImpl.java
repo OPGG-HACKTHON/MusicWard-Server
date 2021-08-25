421d040d55ca04382ddf0a95355103cc4bb8d3ba
leeseojune <sung07288346@gmail.com>
2021-08-25 23:09:36 +0900

    Add :: 댓글 등록 구현

---
+++ src/main/java/io/github/opgg/music_ward_server/service/comment/CommentServiceImpl.java
@@ -0,0 +1,45 @@
+package io.github.opgg.music_ward_server.service.comment;
+
+import io.github.opgg.music_ward_server.dto.comment.request.CommentRequest;
+import io.github.opgg.music_ward_server.entity.comment.Comment;
+import io.github.opgg.music_ward_server.entity.comment.CommentRepository;
+import io.github.opgg.music_ward_server.entity.playlist.Playlist;
+import io.github.opgg.music_ward_server.entity.playlist.PlaylistRepository;
+import io.github.opgg.music_ward_server.entity.user.User;
+import io.github.opgg.music_ward_server.entity.user.UserRepository;
+import io.github.opgg.music_ward_server.exception.PlaylistNotFoundException;
+import io.github.opgg.music_ward_server.exception.UserNotFoundException;
+import io.github.opgg.music_ward_server.utils.security.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final PlaylistRepository playlistRepository;
+
+
+    @Override
+    public void postComment(CommentRequest request) {
+        Long userId = SecurityUtil.getCurrentUserId();
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(UserNotFoundException::new);
+
+        Playlist playlist = playlistRepository
+                .findById(Long.valueOf(request.getPlaylistId()))
+                .orElseThrow(PlaylistNotFoundException::new);
+
+        commentRepository.save(
+                Comment.builder()
+                .user(user)
+                .playlist(playlist)
+                .content(request.getComment())
+                .build()
+        );
+
+    }
+}
