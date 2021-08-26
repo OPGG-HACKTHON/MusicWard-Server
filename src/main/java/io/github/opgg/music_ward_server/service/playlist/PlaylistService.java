4ec0c4d3648ff09d74aa6c0f99da949c1e4feadf
leeseojune <sung07288346@gmail.com>
2021-08-26 23:31:48 +0900

    Update :: 플레이리스트 신고 interface

--- src/main/java/io/github/opgg/music_ward_server/service/playlist/PlaylistService.java
+++ src/main/java/io/github/opgg/music_ward_server/service/playlist/PlaylistService.java
@@ -1,5 +1,6 @@
 package io.github.opgg.music_ward_server.service.playlist;
 
+import io.github.opgg.music_ward_server.dto.playlist.request.PlaylistReportRequest;
 import io.github.opgg.music_ward_server.dto.playlist.request.PlaylistUpdateRequest;
 import io.github.opgg.music_ward_server.dto.playlist.response.NonPlaylistsResponse;
 import io.github.opgg.music_ward_server.dto.playlist.response.PlaylistMainResponse;
@@ -20,4 +21,5 @@ public interface PlaylistService {
     void delete(Long playlistId);
     void addView(Long playlistId);
     List<PlaylistMainResponse> findByUserId(Long userId);
+    void report(PlaylistReportRequest request);
 }
