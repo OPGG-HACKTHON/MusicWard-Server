5896f07f2572a01578158f7f4bb6bee2fa72e42c
leeseojune <sung07288346@gmail.com>
2021-08-26 22:14:01 +0900

    Update :: editComment interface

--- src/main/java/io/github/opgg/music_ward_server/service/comment/CommentService.java
+++ src/main/java/io/github/opgg/music_ward_server/service/comment/CommentService.java
@@ -1,7 +1,9 @@
 package io.github.opgg.music_ward_server.service.comment;
 
 import io.github.opgg.music_ward_server.dto.comment.request.CommentRequest;
+import io.github.opgg.music_ward_server.dto.comment.request.EditCommentRequest;
 
 public interface CommentService {
     void postComment(CommentRequest request);
+    void editComment(EditCommentRequest request);
 }
