c5e63f0d4ad3ba33b9b92092e50e2a47e3b8a9c7
hyeonic <dev.hyeonic@gmail.com>
2021-08-27 20:03:15 +0900

    Add :: champion 별 랭킹 response dto 추가

--- src/main/java/io/github/opgg/music_ward_server/dto/champion/response/ChampionRankingResponse.java
+++ src/main/java/io/github/opgg/music_ward_server/dto/champion/response/ChampionRankingResponse.java
@@ -1,2 +1,23 @@
-package io.github.opgg.music_ward_server.dto.champion.response;public class ChampionRankingResponse {
+package io.github.opgg.music_ward_server.dto.champion.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.github.opgg.music_ward_server.entity.champion.Champion;
+import lombok.Getter;
+
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ChampionRankingResponse {
+
+    private ChampionMainResponse champion;
+    private int view;
+    private int wardsTotal;
+    private int commentsTotal;
+
+    public ChampionRankingResponse(Champion champion, int view, int wardsTotal, int commentsTotal) {
+        this.champion = new ChampionMainResponse(champion);
+        this.view = view;
+        this.wardsTotal = wardsTotal;
+        this.commentsTotal = commentsTotal;
+    }
 }
