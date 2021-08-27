870454aacf3eb86c4b9ff2d29f4b70a3dfc41a9a
hyeonic <dev.hyeonic@gmail.com>
2021-08-27 20:04:12 +0900

    Add :: Ranking 관련 controller 추가 및 권한 허용

--- src/main/java/io/github/opgg/music_ward_server/controller/RankingController.java
+++ src/main/java/io/github/opgg/music_ward_server/controller/RankingController.java
@@ -1,2 +1,29 @@
-package io.github.opgg.music_ward_server.controller;public class RankingController {
+package io.github.opgg.music_ward_server.controller;
+
+import io.github.opgg.music_ward_server.controller.response.CommonResponse;
+import io.github.opgg.music_ward_server.service.champion.ChampionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RequiredArgsConstructor
+@RestController
+public class RankingController {
+
+    private final ChampionService championService;
+
+    @GetMapping("ranking/{rankingType}")
+    public ResponseEntity<CommonResponse> getRanking(@PathVariable("rankingType") String rankingType) {
+
+        if (rankingType.equals("champion")) {
+            return new ResponseEntity<>(new CommonResponse(championService.getRanking()), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(new CommonResponse(null), HttpStatus.OK);
+    }
 }
