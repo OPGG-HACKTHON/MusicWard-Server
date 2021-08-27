55b2a703ba12a4ae6b8a4ca00f8f46061093a86a
hyeonic <dev.hyeonic@gmail.com>
2021-08-27 20:02:39 +0900

    Add :: champion 이름 별 랭킹 조회 추가

--- src/main/java/io/github/opgg/music_ward_server/service/champion/ChampionServiceImpl.java
+++ src/main/java/io/github/opgg/music_ward_server/service/champion/ChampionServiceImpl.java
@@ -3,11 +3,14 @@ package io.github.opgg.music_ward_server.service.champion;
 import io.github.opgg.music_ward_server.dto.champion.response.ChampionDetailDTO;
 import io.github.opgg.music_ward_server.dto.champion.response.ChampionListDTO;
 import io.github.opgg.music_ward_server.dto.champion.response.ChampionListResponse;
+import io.github.opgg.music_ward_server.dto.champion.response.ChampionRankingResponse;
 import io.github.opgg.music_ward_server.entity.champion.Champion;
 import io.github.opgg.music_ward_server.entity.champion.ChampionRepository;
+import io.github.opgg.music_ward_server.entity.playlist.Playlist;
 import io.github.opgg.music_ward_server.exception.ChampionNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,4 +38,37 @@ public class ChampionServiceImpl implements ChampionService {
 
     }
 
+    @Override
+    public List<ChampionRankingResponse> getRanking() {
+
+        return championRepository.findAll()
+                .stream()
+                .map(champion -> {
+                    List<Playlist> playlists = champion.getPlaylists();
+
+                    int view = 0;
+                    int wardsTotal = 0;
+                    int commentsTotal = 0;
+                    for (Playlist playlist : playlists) {
+                        view += playlist.getView();
+                        wardsTotal += playlist.getWards().size();
+                        commentsTotal += playlist.getComments().size();
+                    }
+
+                    return new ChampionRankingResponse(champion, view, wardsTotal, commentsTotal);
+                })
+                .sorted(((o1, o2) -> {
+
+                    int total1 = o1.getView() + o1.getWardsTotal() + o1.getCommentsTotal();
+                    int total2 = o2.getView() + o2.getWardsTotal() + o2.getCommentsTotal();
+
+                    if (total1 < total2) {
+                        return 1;
+                    }
+
+                    return -1;
+                }))
+                .limit(10)
+                .collect(Collectors.toList());
+    }
 }
