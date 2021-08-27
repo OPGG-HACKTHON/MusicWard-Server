52bb00aaf444a3929064958da33472ad0034e2af
leeseojune <sung07288346@gmail.com>
2021-08-27 13:49:30 +0900

    Update :: Validator 적용

--- src/main/java/io/github/opgg/music_ward_server/dto/comment/request/CommentRequest.java
+++ src/main/java/io/github/opgg/music_ward_server/dto/comment/request/CommentRequest.java
@@ -5,11 +5,14 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CommentRequest {
 
+    @NotNull(message = "comment는 null을 허용하지 않습니다.")
     private String comment;
     private long playlistId;
 
--- src/main/java/io/github/opgg/music_ward_server/dto/user/request/RefreshTokenRequest.java
+++ src/main/java/io/github/opgg/music_ward_server/dto/user/request/RefreshTokenRequest.java
@@ -5,11 +5,14 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class RefreshTokenRequest {
 
+    @NotEmpty(message = "refresh token은 null과 공백을 허용하지 않습니다.")
     private String refreshToken;
 
 }
