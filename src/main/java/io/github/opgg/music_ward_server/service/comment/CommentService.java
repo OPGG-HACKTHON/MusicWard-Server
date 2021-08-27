014f03bf644fea0e3f0d52e03e25d178445604e6
leeseojune <sung07288346@gmail.com>
2021-08-27 10:13:36 +0900

    Update :: 댓글삭제 interface

--- src/main/java/io/github/opgg/music_ward_server/service/comment/CommentService.java
+++ src/main/java/io/github/opgg/music_ward_server/service/comment/CommentService.java
@@ -2,8 +2,10 @@ package io.github.opgg.music_ward_server.service.comment;
 
 import io.github.opgg.music_ward_server.dto.comment.request.CommentRequest;
 import io.github.opgg.music_ward_server.dto.comment.request.EditCommentRequest;
+import io.github.opgg.music_ward_server.dto.comment.request.RemoveCommentRequest;
 
 public interface CommentService {
     void postComment(CommentRequest request);
     void editComment(EditCommentRequest request);
+    void removeComment(RemoveCommentRequest request);
 }
