96615630aafffe056d9df4b672c0c2e973386876
leeseojune <sung07288346@gmail.com>
2021-08-27 11:06:31 +0900

    Update :: Optional<Ward> findByUserAndPlaylist

--- src/main/java/io/github/opgg/music_ward_server/entity/ward/WardRepository.java
+++ src/main/java/io/github/opgg/music_ward_server/entity/ward/WardRepository.java
@@ -1,12 +1,16 @@
 package io.github.opgg.music_ward_server.entity.ward;
 
+import io.github.opgg.music_ward_server.entity.playlist.Playlist;
 import io.github.opgg.music_ward_server.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface WardRepository extends JpaRepository<Ward, Long> {
     Integer countByPlaylistId(Long playlistId);
+    Optional<Ward> findByUserAndPlaylist(User user, Playlist playlist);
     void deleteByPlaylistId(Long playlistId);
     void deleteByUser(User user);
 }
