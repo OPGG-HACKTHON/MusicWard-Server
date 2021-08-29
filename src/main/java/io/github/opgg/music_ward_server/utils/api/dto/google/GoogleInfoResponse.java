d449179c25c082c43ab06657cf810c547322dd12
leeseojune <sung07288346@gmail.com>
2021-08-29 23:09:50 +0900

    Update :: GoogleInfoResponse

--- src/main/java/io/github/opgg/music_ward_server/utils/api/dto/google/GoogleInfoResponse.java
+++ src/main/java/io/github/opgg/music_ward_server/utils/api/dto/google/GoogleInfoResponse.java
@@ -3,8 +3,9 @@ package io.github.opgg.music_ward_server.utils.api.dto.google;
 import lombok.Getter;
 
 @Getter
-public class GoogleEmailResponse {
+public class GoogleInfoResponse {
 
     private String email;
+    private String name;
 
 }
