549017ef45af1252434439d9fa402686d2a5b04d
DESKTOP-PNLTC14\IRC <save3172@naver.com>
2021-08-30 13:22:17 +0900

    Add :: Riot API response dto 추가

---
+++ src/main/java/io/github/opgg/music_ward_server/utils/api/dto/riot/RiotSummonerResponse.java
@@ -0,0 +1,18 @@
+package io.github.opgg.music_ward_server.utils.api.dto.riot;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RiotSummonerResponse {
+    private String accountId;
+    private Integer profileIconId;
+    private Long revisionDate;
+    private String name;
+    private String id;
+    private String puuid;
+    private Long summonerLevel;
+}
