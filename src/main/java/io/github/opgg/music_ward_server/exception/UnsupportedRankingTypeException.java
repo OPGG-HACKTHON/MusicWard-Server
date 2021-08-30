9942d831834e2975d113319660c2cdc8bd1b9acb
hyeonic <dev.hyeonic@gmail.com>
2021-08-30 17:03:19 +0900

    Add :: Unsupported Ranking type 예외 추가

--- src/main/java/io/github/opgg/music_ward_server/exception/UnsupportedRankingTypeException.java
+++ src/main/java/io/github/opgg/music_ward_server/exception/UnsupportedRankingTypeException.java
@@ -1,2 +1,11 @@
-package io.github.opgg.music_ward_server.exx;public class UnsupportedRankingTypeException {
+package io.github.opgg.music_ward_server.exception;
+
+import io.github.opgg.music_ward_server.error.exception.ErrorCode;
+import io.github.opgg.music_ward_server.error.exception.MusicWardException;
+
+public class UnsupportedRankingTypeException extends MusicWardException {
+
+    public UnsupportedRankingTypeException() {
+        super(ErrorCode.UNSUPPORTED_RANKING_TYPE);
+    }
 }
