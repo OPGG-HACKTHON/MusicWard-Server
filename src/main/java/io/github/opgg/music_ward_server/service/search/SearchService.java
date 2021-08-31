ed5a2439b89a8f20f597a1e0351f87fb50ab14d1
hyeonic <dev.hyeonic@gmail.com>
2021-08-31 19:48:41 +0900

    Add :: searchService champion name 검색 추가

--- src/main/java/io/github/opgg/music_ward_server/service/search/SearchService.java
+++ src/main/java/io/github/opgg/music_ward_server/service/search/SearchService.java
@@ -1,7 +1,13 @@
 package io.github.opgg.music_ward_server.service.search;
 
+import io.github.opgg.music_ward_server.dto.playlist.response.PlaylistMainResponse;
 import io.github.opgg.music_ward_server.dto.search.response.SearchSummonerResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 
 public interface SearchService {
+
     SearchSummonerResponse getRiotSummonerInfo(String summonerName);
+    Page<PlaylistMainResponse> findByChampionName(String championName, Pageable pageable);
 }
--- src/main/java/io/github/opgg/music_ward_server/service/search/SearchServiceImpl.java
+++ src/main/java/io/github/opgg/music_ward_server/service/search/SearchServiceImpl.java
@@ -1,12 +1,22 @@
 package io.github.opgg.music_ward_server.service.search;
 
+import io.github.opgg.music_ward_server.dto.playlist.response.PlaylistMainResponse;
 import io.github.opgg.music_ward_server.dto.search.response.SearchSummonerResponse;
+import io.github.opgg.music_ward_server.entity.comment.CommentRepository;
+import io.github.opgg.music_ward_server.entity.playlist.Playlist;
+import io.github.opgg.music_ward_server.entity.playlist.PlaylistRepository;
+import io.github.opgg.music_ward_server.entity.tag.Tag;
+import io.github.opgg.music_ward_server.entity.tag.TagRepository;
+import io.github.opgg.music_ward_server.entity.track.TrackRepository;
+import io.github.opgg.music_ward_server.entity.ward.WardRepository;
 import io.github.opgg.music_ward_server.utils.api.client.riot.RiotMatchClient;
 import io.github.opgg.music_ward_server.utils.api.client.riot.RiotSummonerClient;
 import io.github.opgg.music_ward_server.utils.api.dto.riot.RiotMatchDetailResponse;
 import io.github.opgg.music_ward_server.utils.api.dto.riot.RiotSummonerResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +25,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -23,6 +34,11 @@ public class SearchServiceImpl implements SearchService {
 
     private final RiotSummonerClient riotSummonerClient;
     private final RiotMatchClient riotMatchClient;
+    private final TagRepository tagRepository;
+    private final WardRepository wardRepository;
+    private final TrackRepository trackRepository;
+    private final CommentRepository commentRepository;
+    private final PlaylistRepository playlistRepository;
 
     @Value("${api.riot.key}")
     private String riotAPIKey;
@@ -34,6 +50,26 @@ public class SearchServiceImpl implements SearchService {
         List<String> matchList = riotMatchClient.getMatch(riotAPIKey, userPuuid);
         SearchSummonerResponse searchSummonerResponse = getSearchSummonerResponse(matchList, summonerName, userPuuid);
         return searchSummonerResponse;
+    }
+
+    @Override
+    public Page<PlaylistMainResponse> findByChampionName(String championName, Pageable pageable) {
+
+        Page<Playlist> playlists = playlistRepository.findByChampionNameContaining(championName, pageable);
+        Page<PlaylistMainResponse> playlistMainResponses = playlists.map(playlist -> {
+            List<Tag> findTags = tagRepository.findByPlaylistId(playlist.getId());
+            List<String> tags = findTags.stream()
+                    .map(tag -> tag.getTitle())
+                    .collect(Collectors.toList());
+
+            Integer wardTotal = wardRepository.countByPlaylistId(playlist.getId());
+            Integer commentTotal = commentRepository.countByPlaylistId(playlist.getId());
+            Integer trackTotal = trackRepository.countByPlaylistId(playlist.getId());
+
+            return new PlaylistMainResponse(playlist, tags, wardTotal, commentTotal, trackTotal);
+        });
+
+        return playlistMainResponses;
     }
 
     private SearchSummonerResponse getSearchSummonerResponse(
