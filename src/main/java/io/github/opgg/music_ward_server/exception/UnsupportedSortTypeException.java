635fe9da348d280dc157190ee875b20228857237
hyeonic <dev.hyeonic@gmail.com>
2021-09-01 19:18:41 +0900

    Add :: 예외 추가

--- src/main/java/io/github/opgg/music_ward_server/exception/UnsupportedSortTypeException.java
+++ src/main/java/io/github/opgg/music_ward_server/exception/UnsupportedSortTypeException.java
@@ -1,2 +1,11 @@
-package io.github.opgg.music_ward_server.exception;public class UnsupportedSortTypeException {
+package io.github.opgg.music_ward_server.exception;
+
+import io.github.opgg.music_ward_server.error.exception.ErrorCode;
+import io.github.opgg.music_ward_server.error.exception.MusicWardException;
+
+public class UnsupportedSortTypeException extends MusicWardException {
+
+    public UnsupportedSortTypeException() {
+        super(ErrorCode.UNSUPPORTED_SORT_TYPE);
+    }
 }
