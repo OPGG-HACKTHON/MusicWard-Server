ac33502c260a17d089eeda20c1f14a2e4fa4a842
leeseojune <sung07288346@gmail.com>
2021-09-09 23:28:18 +0900

    FIX :: .

--- src/main/java/io/github/opgg/music_ward_server/config/WebConfig.java
+++ src/main/java/io/github/opgg/music_ward_server/config/WebConfig.java
@@ -1,17 +1,16 @@
 package io.github.opgg.music_ward_server.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedOrigins("http://localhost:3000", "https://site.music-ward.com");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+//                .allowedOrigins("http://localhost:3000", "https://site.music-ward.com");
+//    }
 
 }
