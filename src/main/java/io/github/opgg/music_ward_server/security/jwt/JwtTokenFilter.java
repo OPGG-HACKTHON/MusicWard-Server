48983707438024c062725d0defa9a8156bd0f5b3
leeseojune <sung07288346@gmail.com>
2021-09-09 23:13:43 +0900

    FIX :: response에 header...

--- src/main/java/io/github/opgg/music_ward_server/security/jwt/JwtTokenFilter.java
+++ src/main/java/io/github/opgg/music_ward_server/security/jwt/JwtTokenFilter.java
@@ -22,16 +22,18 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String token = jwtTokenProvider.resolveToken(request);
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            Authentication authentication = jwtTokenProvider.authentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
-		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+
+		String token = jwtTokenProvider.resolveToken(request);
+		if (token != null && jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.authentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
 
         filterChain.doFilter(request, response);
     }
