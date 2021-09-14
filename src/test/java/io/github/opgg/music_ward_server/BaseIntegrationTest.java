3107de3847a856ba48a192ffcea81736a514a90a
hyeonic <dev.hyeonic@gmail.com>
2021-09-14 19:25:11 +0900

    Add :: Base test @Transaction 추가

--- src/test/java/io/github/opgg/music_ward_server/BaseIntegrationTest.java
+++ src/test/java/io/github/opgg/music_ward_server/BaseIntegrationTest.java
@@ -10,11 +10,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootTest
 @Disabled
+@Transactional
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
