59b4a69205ff085a74f973d2a71ec758e179f15a
leeseojune <sung07288346@gmail.com>
2021-09-14 18:09:18 +0900

    Update :: Builder 추가

--- src/main/java/io/github/opgg/music_ward_server/dto/comment/request/RemoveCommentRequest.java
+++ src/main/java/io/github/opgg/music_ward_server/dto/comment/request/RemoveCommentRequest.java
@@ -2,11 +2,15 @@ package io.github.opgg.music_ward_server.dto.comment.request;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class RemoveCommentRequest {
 
