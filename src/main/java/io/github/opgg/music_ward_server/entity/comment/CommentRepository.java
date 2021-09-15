c7206c9d81fcc32cc1135047d7676d23745973bb
hyeonic <dev.hyeonic@gmail.com>
2021-09-15 22:18:20 +0900

    Update :: playlist service 수정

--- src/main/java/io/github/opgg/music_ward_server/entity/comment/CommentRepository.java
+++ src/main/java/io/github/opgg/music_ward_server/entity/comment/CommentRepository.java
@@ -10,4 +10,5 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findByPlaylistId(Long playlistId);
     Integer countByPlaylistId(Long playlistId);
+    void deleteByPlaylistId(Long playlistId);
 }
