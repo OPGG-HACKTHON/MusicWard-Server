b79f9050db48d87c1816d7cdb3572b7fcdc16f4b
leeseojune <sung07288346@gmail.com>
2021-09-16 21:36:40 +0900

    Update :: deleteByIdStartingWith

--- src/main/java/io/github/opgg/music_ward_server/entity/token/TokenRepository.java
+++ src/main/java/io/github/opgg/music_ward_server/entity/token/TokenRepository.java
@@ -1,11 +1,12 @@
 package io.github.opgg.music_ward_server.entity.token;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface TokenRepository extends CrudRepository<Token, String> {
     Optional<Token> findByRefreshToken(String refreshToken);
+    void deleteByIdStartingWith(String id);
 }
